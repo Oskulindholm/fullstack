c26a5cff70792ca7865844df3b5c75e022c0c549
Oskulindholm <lindholm.osku@gmail.com>
2023-04-16 16:27:43 +0300

    Tehtävä 1.2

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -34,15 +34,22 @@ const Header = ({course}) => {
 const Content = ({part1, part2, part3, exercises1, exercises2, exercises3}) => {
   return (
     <>
-    <p>
-      {part1} {exercises1}
-    </p>
-    <p>
-      {part2} {exercises2}
-    </p>
-    <p>
-      {part3} {exercises3}
-    </p>
+      <Part part={part1} exercises={exercises1} />
+      <Part part={part2} exercises={exercises2} />
+      <Part part={part3} exercises={exercises3} />
+    </>
+  )
+}
+
+
+/// PART component renders the name of a part and number of exercises in it.
+const Part = ({part, exercises}) => {
+
+  return (
+    <>
+      <p>
+        {part} {exercises}
+      </p>
     </>
   )
 }
