9152a73756083ebf30dd4766dca5bce69641ca8c
Oskulindholm <lindholm.osku@gmail.com>
2023-04-16 16:31:34 +0300

    Tehtävä 1.3

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -2,18 +2,24 @@
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Content part1={part1} part2={part2} part3={part3} />
+      <Total part1={part1} part2={part2} part3={part3} />
     </div>
   )
 }
@@ -31,24 +37,24 @@ const Header = ({course}) => {
 
 
 /// CONTENT component renders the main content of the web page.
-const Content = ({part1, part2, part3, exercises1, exercises2, exercises3}) => {
+const Content = ({part1, part2, part3}) => {
   return (
     <>
-      <Part part={part1} exercises={exercises1} />
-      <Part part={part2} exercises={exercises2} />
-      <Part part={part3} exercises={exercises3} />
+      <Part name={part1.name} exercises={part1.exercises} />
+      <Part name={part2.name} exercises={part2.exercises} />
+      <Part name={part3.name} exercises={part3.exercises} />
     </>
   )
 }
 
 
 /// PART component renders the name of a part and number of exercises in it.
-const Part = ({part, exercises}) => {
+const Part = ({name, exercises}) => {
 
   return (
     <>
       <p>
-        {part} {exercises}
+        {name} {exercises}
       </p>
     </>
   )
@@ -56,10 +62,10 @@ const Part = ({part, exercises}) => {
 
 
 /// TOTAL component renders the total amount of exercises in the course page.
-const Total = ({exercises1, exercises2, exercises3}) => {
+const Total = ({part1, part2, part3}) => {
   return (
     <>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <p>Number of exercises {part1.exercises + part2.exercises + part3.exercises}</p>
     </>
   )
 }
