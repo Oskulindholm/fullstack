8877dd44548cea77e037566bc9ae9c70560052bb
Oskulindholm <lindholm.osku@gmail.com>
2023-04-19 18:28:01 +0300

    Tehtävä 2.11

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,15 +1,20 @@
-import { useState } from 'react'
+import axios from 'axios'
+
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(res => {
+        setPersons(res.data)
+      })
+  }, [])
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -99,7 +104,7 @@ const PersonList = ({persons, filter}) => {
   return (
     <>
     {persons.filter(p => p.name.toLowerCase().includes(filter.toLowerCase()))
-      .map(p => <p key={p.name}> {p.name} {p.number}</p>)}
+      .map(p => <p key={p.id}> {p.name} {p.number}</p>)}
     </>
   )
 }
