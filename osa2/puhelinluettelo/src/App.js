d49bfb2787525c2446d161d6a61bd57209916c90
Oskulindholm <lindholm.osku@gmail.com>
2023-04-19 16:29:08 +0300

    Tehtävä 2.8

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,14 +2,18 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }, 
-    { name: 'Grace Hopper'}
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
     const name = newName.trim()
+    const number = newNumber.trim()
 
     if (persons.find(p => p.name.trim().toLowerCase() === newName.trim().toLowerCase())) {
       window.alert(`${name} is already added to the phonebook.`)
@@ -17,11 +21,13 @@ const App = () => {
     }
 
     const newPerson = {
-      name: name
+      name: name,
+      number: number
     }
   
     setPersons(persons.concat(newPerson))
     setNewName('')
+    setNewNumber('')
 
     console.log(newPerson.name + ' added')
   }
@@ -30,20 +36,27 @@ const App = () => {
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
         <div>
-          name: <input value={newName} onChange={handleNameChange}/>
+          Name: <input value={newName} onChange={handleNameChange}/>
         </div>
         <div>
-          <button type="submit">add</button>
+          Number: <input value={newNumber} onChange={handleNumberChange}/>
+        </div>
+        <div>
+          <button type="submit">Add</button>
         </div>
       </form>
 
       <h2>Numbers</h2>
-        {persons.map(p => <p key={p.name}> {p.name} </p>)}
+        {persons.map(p => <p key={p.name}> {p.name} {p.number}</p>)}
     </div>
   )
 
