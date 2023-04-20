728937c79240bd7281639187b698e13bbced33f3
Oskulindholm <lindholm.osku@gmail.com>
2023-04-20 14:18:09 +0300

    Tehtävä 2.18

---
+++ osa2/maiden_tiedot/src/App.js
@@ -0,0 +1,88 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const App = () => {
+
+  const [countries, setCountries] = useState([])
+  const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('https://restcountries.com/v3.1/all')
+      .then(res => {
+        setCountries(res.data)
+      })
+  }, [])
+
+  const handleFilter = (event) => {
+    setFilter(event.target.value.trim())
+}
+
+
+  return (
+    <div>
+      Find countries: <input onChange={handleFilter} /> <br/>
+
+      <CountryList filter={filter} countries={countries} />
+    </div>
+  )
+}
+
+/// COUNTRYLIST component renders the list of all countries that match the filter.
+const CountryList = ({filter, countries}) => {
+
+  const matches = countries.filter(c => c.name.common.toLowerCase().includes(filter.toLowerCase()))
+
+  if (matches.length > 10) {
+        return (
+            <>
+              Too many matches, specify another filter.
+            </>
+        )
+    } else if (matches.length > 1) {
+        return (
+            <>
+              {matches.map(c => <p key={c.name.common}> {c.name.common} </p>)}
+            </>
+        )
+    } else if (matches.length == 1 ) {
+        return (
+            <>
+              <Country country={matches[0]} />
+            </>
+        )
+    } else {
+        return (
+            <>No countries match the current search term.</>
+        )
+    }
+}
+
+
+/// COUNTRY component displays the basic information related to a single country
+const Country = ({country}) => {
+
+    const languages = Object.keys(country.languages).map(key => {return country.languages[key]})
+    const lat = country.capitalInfo.latlng[0]
+    const lon = country.capitalInfo.latlng[1]
+
+
+    return (
+      <>
+        <h2>{country.name.common}</h2>
+        <p>
+          Capital: {country.capital}<br/>
+          Area: {country.area} km2
+        </p>
+
+        <h3>Languages</h3>
+        <ul>
+            {languages.map(l => <li key={l}>{l}</li>) }
+        </ul>
+        
+        <img src={country.flags.png} />
+      </>
+    )
+}
+
+export default App
